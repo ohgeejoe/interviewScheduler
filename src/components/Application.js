32f36b1c6f32cb364b6f437237faad65158649d9
Joseph Shin <seoung.shin@ontariotechu.net>
2021-11-01 16:21:25 -0400

    implement getInterview helper

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 import Appointment from "components/Appointment/index.js";
 import { useEffect } from "react";
 import axios from 'axios';
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 
@@ -27,10 +27,10 @@ export default function Application(props) {
     Promise.all([
     axios.get(`http://localhost:8001/api/days`),
     axios.get('http://localhost:8001/api/appointments'),
-    // axios.get('http://localhost:8001/api/interviewers')
+    axios.get('http://localhost:8001/api/interviewers')
     ])
     .then(res => {
-      setState(prev => ({...prev, days: res[0].data, appointments: res[1].data }));
+      setState(prev => ({...prev, days: res[0].data, appointments: res[1].data, interviewers: res[2].data }));
     })
     
     },[])
@@ -58,10 +58,9 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
        
-        {dailyAppointments.map((appointment) => <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
+        {dailyAppointments.map((appointment) => 
+        <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={getInterview(state, appointment.interview)} />
         
         )}
         <Appointment key="last" time="5pm" />
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,3 +18,17 @@ export function getAppointmentsForDay(state, day) {
   
   return appointmentsArr;
 }
+
+export function getInterview(state, interview) {
+ //since some appointment slots will not have an interview.
+  if (!interview) {
+  return null;
+  }
+  return (
+    {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer]
+    }
+  )
+
+}
