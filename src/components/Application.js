5525c24564ed92d075bf9fd7c2f2e06dff329b2f
Joseph Shin <seoung.shin@ontariotechu.net>
2021-11-07 17:07:58 -0500

    comments removed

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,84 +20,8 @@ export default function Application(props) {
     editInterview
   } = useApplicationData();
 
-//   function cancelInterview(id) {
-
-//     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-//       .then((res) => {
-//         const appointment = {
-//           ...state.appointments[id],
-//           interview: null
-//         };
-    
-//         const appointments = {
-//           ...state.appointments,
-//           [id]: appointment
-//         };
-  
-//         setState({
-//           ...state,
-//           appointments
-//         });
-//   })
-// };
-
-// function editInterview(id, interview) {
-
-//   const appointment = {
-//     ...state.appointments[id],
-//     interview: { ...interview }
-//   };
-
-//   const appointments = {
-//     ...state.appointments,
-//     [id]: appointment
-//   };
-
-//   setState({
-//     ...state,
-//     appointments
-//   });
-
-//   return axios.put(`http://localhost:8001/api/appointments/${id}`, {
-//     ...state.appointments[id],
-//     interview: { ...interview }
-//   })
-// }
-
-//   const [state, setState] = useState({
-//     day: "Monday",
-//     days: [],
-//     appointments: {}
-//   });
-
-//   function bookInterview(id, interview) {
-
-//     const appointment = {
-//       ...state.appointments[id],
-//       interview: { ...interview }
-//     };
-
-//     const appointments = {
-//       ...state.appointments,
-//       [id]: appointment
-//     };
-
-//     setState({
-//       ...state,
-//       appointments
-//     });
-
-//     return axios.put(`http://localhost:8001/api/appointments/${id}`, {
-//       ...state.appointments[id],
-//       interview: { ...interview }
-//     })
-//   }
-
-
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  
-  // const setDay = day => setState({...state, day});
 
   useEffect(() => {
     Promise.all([
